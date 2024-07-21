63a99453c4de9d249dc878d4c8e6932bb82582ed
chianyungcode <cnytechcode@gmail.com>
2024-07-21 11:45:25 +0700

    feat: add CORS middleware to enable cross-origin resource sharing

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
+import { cors } from "hono/cors";
 
 import auth from "./routes/auth";
 import categories from "./routes/categories";
@@ -7,6 +8,17 @@ import products from "./routes/products";
 
 const app = new Hono().basePath("/api");
 app.use(logger());
+app.use(
+  "*",
+  cors({
+    origin: ["http://localhost:5173"],
+    allowHeaders: ["X-Custom-Header", "Upgrade-Insecure-Requests"],
+    allowMethods: ["POST", "GET", "PUT", "DELETE"],
+    exposeHeaders: ["Content-Length", "X-Kuma-Revision"],
+    maxAge: 600,
+    credentials: true,
+  })
+);
 
 app.get("/", (c) => {
   console.log(process.env.DATABASE_URL);

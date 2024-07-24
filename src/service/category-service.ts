b3de2d5557297beb56d0437a13e90be44abef1ab
chianyungcode <cnytechcode@gmail.com>
2024-07-24 16:27:25 +0700

    refactor: modify what return is returned from getCategories

--- src/service/category-service.ts
+++ src/service/category-service.ts
@@ -1,10 +1,14 @@
 import { prisma } from "../../prisma/client";
-import { Prisma } from "@prisma/client";
 import { CategoryValidation } from "../validation/category-validation";
-import { ProductValidation } from "../validation/product-validation";
 import { z } from "zod";
 
 export class CategoryService {
+  /**
+   * Creates a new category.
+   *
+   * @param category - The category data to create.
+   * @returns The newly created category.
+   */
   static async create(category: z.infer<typeof CategoryValidation.create>) {
     const { name, slug, description } = category;
 
@@ -19,26 +23,56 @@ export class CategoryService {
     return newCategory;
   }
 
+  /**
+   * Retrieves all categories including their products and images.
+   *
+   * @returns A list of categories with their associated products and images.
+   */
   static async getCategories() {
     const categories = await prisma.category.findMany({
       include: {
-        products: true,
+        products: {
+          include: {
+            images: true,
+          },
+        },
       },
     });
 
     return categories;
   }
 
+  /**
+   * Retrieves a category by its slug, including its products and images.
+   *
+   * @param slug - The unique slug of the category to retrieve.
+   * @returns A promise that resolves to the category with its associated products and images.
+   * @throws {Prisma.PrismaClientKnownRequestError} If the category is not found.
+   */
   static async getCategory(slug: string) {
     const category = await prisma.category.findUniqueOrThrow({
       where: {
         slug,
+      },
+      include: {
+        products: {
+          include: {
+            images: true,
+          },
+        },
       },
     });
 
     return category;
   }
 
+  /**
+   * Updates an existing category.
+   *
+   * @param id - The ID of the category to update.
+   * @param updatedCategory - The updated category data.
+   * @returns The updated category.
+   */
   static async update(
     id: string,
     updatedCategory: z.infer<typeof CategoryValidation.update>
@@ -51,6 +85,11 @@ export class CategoryService {
     return category;
   }
 
+  /**
+   * Deletes a category by its ID.
+   *
+   * @param id - The ID of the category to delete.
+   */
   static async deleteCategory(id: string) {
     await prisma.category.delete({
       where: {
@@ -59,6 +98,9 @@ export class CategoryService {
     });
   }
 
+  /**
+   * Deletes all categories.
+   */
   static async deleteAllCategories() {
     await prisma.category.deleteMany();
   }
